41f04d5109ee0294d30bafd4e16783326fedf065
Joseph El-Khouri <joseph.elkhouri+github@gmail.com>
2016-10-29 01:42:35 -0400

    i love me some arrows

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
   res.locals.title     = "Express"
   res.locals.developer = Math.random() < 0.5 ? "Colin" : "Joseph"
   res.render('index', res.locals)
