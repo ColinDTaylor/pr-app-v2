c46ec02274e5c963a5e6bf36fe01bda2cedd5864
Absolome <absolome@gmail.com>
2016-11-04 20:56:24 -0400

    wreckt

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router()
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.locals.title = 'Express'
-  res.locals.developer = Math.random() < 0.5 ? 'Colin' : 'Joseph'
+  // res.locals.developer = Math.random() < 0.5 ? 'Colin' : 'Joseph'
+  res.locals.developer = 'August'
   res.render('index', res.locals)
 })
 
